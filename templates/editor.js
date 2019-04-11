f897c8b8e8441636a6d6e12dfb16e518e7386b1f
nafi <nafiul.bzsb@gmail.com>
2019-04-11 00:46:10 -0400

    merge

---
+++ templates/editor.js
@@ -0,0 +1,39 @@
+var canvas = new fabric.Canvas('c');
+// var rect = new fabric.Rect();
+//
+//
+//
+// canvas.add(rect); // add Object
+//
+// canvas.item(0);
+// canvas.getObjects();
+
+function pencil() {
+    canvas.isDrawingMode = true;
+    canvas.freeDrawingBrush.width = 2;
+    canvas.freeDrawingBrush.color = "green";
+}
+
+function eraser() {
+
+}
+
+function bucket() {
+
+}
+
+function select() {
+
+}
+
+function circle() {
+
+}
+
+function rectangle() {
+
+}
+
+function triangle() {
+
+}
