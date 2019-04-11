6bad3baa28256bf75d2f78421b758a1ad6bed11a
DESKTOP-DETLFUP\alexa <alexandre.donnellyvaz@stonybrook.edu>
2019-04-10 23:06:14 -0400

    New branch

--- comicapp/src/main/resources/static/js/src/editor.js
+++ comicapp/src/main/resources/static/js/src/editor.js
@@ -1,16 +1,16 @@
 
 var canvas = new fabric.Canvas('myCanvas');
-var rect = new fabric.Rect();
-
-rect
-
-canvas.add(rect); // add Object
 
 canvas.item(0);
 canvas.getObjects();
 
+var color = "black";
+var brushWidth = 2;
+
 function pencil() {
     fabric.isDrawingMode = true;
+    canvas.freeDrawingBrush.width = brushWidth;
+    canvas.freeDrawingBrush.color = color;
 }
 
 function eraser() {
@@ -26,13 +26,42 @@ function select() {
 }
 
 function circle() {
-
+    var circle = new fabric.Circle({
+      radius: 20, fill: color, left: 100, top: 100
+    });
+    canvas.add(circle);
 }
 
 function rectangle() {
-
+    var rect = new fabric.Rect({
+        width: 10, height: 20,
+        left: 100, top: 100,
+        fill: color,
+        angle: 0
+      });
+    canvas.add(rect); // add Object
 }
 
 function triangle() {
+    var triangle = new fabric.Triangle({
+      width: 20, height: 30, fill: color, left: 50, top: 50
+    });
+    canvas.add(triangle);
+}
 
+function setWidth(newWidth) {
+    width = newWidth;
+}
+
+function setColor(newColor) {
+    color = newColor;
+}
+
+function export() {
+    holder = canvas.toJSON();
+    canvas.clear();
+}
+
+function import() {
+    canvas.loadFromJSON(holder);
 }
