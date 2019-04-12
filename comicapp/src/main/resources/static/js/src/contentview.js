ed9d09c895f3786274739bb534d1d352802b5615
nafi <nafiul.bzsb@gmail.com>
2019-04-12 10:41:19 -0400

    comment box style is improved. addcomment is yet to come!

---
+++ comicapp/src/main/resources/static/js/src/contentview.js
@@ -0,0 +1,14 @@
+function addComment(){
+    var comments = document.getElementById("comments");
+    var comment = document.getElementById("newcomment").value;
+    comments.append(
+        "            <div class=\"commenterImage\">\n" +
+        "              <img src=\"http://placekitten.com/50/50\" />\n" +
+        "            </div>\n" +
+        "            <div class=\"commentText\">\n" +
+        "              <p class=\"\">fun text</p> <span class=\"date sub-text\">on March 5th, 2014</span>\n" +
+        "\n" +
+        "            </div>\n"
+    );
+
+}
