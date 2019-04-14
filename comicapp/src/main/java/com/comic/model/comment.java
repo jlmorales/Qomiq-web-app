b72fa72aee539190979fcb3d600adc65c83aafc3
Rongan Li <rongan.li@stonybrook.edu>
2019-04-13 20:22:34 -0400

    added java class on model to create more table

---
+++ comicapp/src/main/java/com/comic/model/comment.java
@@ -0,0 +1,25 @@
+package com.comic.model;
+
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "comment")
+public class comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "comment_Id")
+    private int commentId;
+
+    @Column(name="profileUsername")
+    private String profileUsername;
+
+    @Column(name="commentorUsername")
+    private  String commentorUsername;
+}
