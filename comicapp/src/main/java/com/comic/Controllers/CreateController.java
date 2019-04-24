fd8f8de3ff8a647f9d2845cc976fb73127a26f2a
Rongan Li <rongan.li@stonybrook.edu>
2019-04-24 15:50:47 -0400

    fixed the loading of series and comic on config tag

--- comicapp/src/main/java/com/comic/Controllers/CreateController.java
+++ comicapp/src/main/java/com/comic/Controllers/CreateController.java
@@ -1,4 +1,5 @@
 package com.comic.Controllers;
+import com.amazonaws.services.apigateway.model.Model;
 import com.comic.Service.S3Services;
 import com.comic.Service.SeriesService;
 import com.comic.Service.UserService;
@@ -8,14 +9,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
 @Controller
 public class CreateController {
+
 
     @Autowired
     UserService userService;
@@ -26,18 +33,18 @@ public class CreateController {
     @Autowired
     S3Services s3Services;
 
-
     @RequestMapping(value = {"/create"}, method = RequestMethod.GET)
     public ModelAndView create() {
         ModelAndView modelAndView = new ModelAndView();
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        User user = userService.findUserByUsername(auth.getName());
-        List<Series> series = seriesService.findAllSeriesByAuthorUsername(auth.getName());
+        User user = userService.findUserByEmail(auth.getName());
+        List<Series> series = seriesService.findAllSeriesByAuthorUsername(user.username);
         modelAndView.setViewName("editor");
         modelAndView.addObject("currentUser", user);
         modelAndView.addObject("series", series);
         return modelAndView;
     }
+
 
 
     public void upload(String bucketName, String fileName, String filePath) {
